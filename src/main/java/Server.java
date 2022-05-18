79afab9050c1756d2b91ba9b24c5f15fdbeb1543
Малгатаев Андрей <i-am-malgataev@yandex.ru>
2022-05-18 23:48:26 +0300

    Changes

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -3,31 +3,34 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         int port = 8080;
 
-        while (true) {
-            ServerSocket serverSocket = new ServerSocket(port);
-            Socket connection = serverSocket.accept();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
 
-            PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            while (true) {
+                Socket connection = serverSocket.accept();
 
-            System.out.printf("New connection accepted. Port: %d%n", connection.getPort());
-            out.println("Write your name.");
-            final String name = in.readLine();
+                PrintWriter out = new PrintWriter(connection.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
 
-            out.println("Are you child? (yes/no)");
-            final boolean isChild = in.readLine().equals("yes");
+                System.out.printf("New connection accepted. Port: %d%n", connection.getPort());
+                out.println("Write your name.");
+                final String name = in.readLine();
 
-            if (isChild) {
-                out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
-            } else {
-                out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!"
-                        , name));
+                out.println("Are you child? (yes/no)");
+                final boolean isChild = in.readLine().equals("yes");
+
+                if (isChild) {
+                    out.println(String.format("Welcome to the kids area, %s! Let's play!", name));
+                } else {
+                    out.println(String.format("Welcome to the adult zone, %s! Have a good rest, or a good working day!"
+                            , name));
+                }
             }
-
-            serverSocket.close();
+        } catch (
+                IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
